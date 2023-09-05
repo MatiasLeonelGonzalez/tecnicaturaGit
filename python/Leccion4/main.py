040b311687140172b4f096da1f0db98ed4aa9eee
Matias Gonzalez <Skrowyy@gmail.com>
2023-09-05 02:51:54 -0300

    clase 1 terminada y comitiada

--- python/Leccion4/main.py
+++ python/Leccion4/main.py
@@ -69,8 +69,47 @@ print(nombres)
 # Eliminar la lista
 
 del nombres
-print(nombres)  # nos salta un error por que ya borro la lista!
+# print(nombres)  # nos salta un error por que ya borro la lista!
 
 # video 4 en EjercicioRango.py
 
-# video 5
+# video 6
+
+# Definimos una tupla
+cocina = "cuchara", "cuchillo", "tenedor"
+print(cocina)
+print(len(cocina))
+
+# Acceder a un elemento,para esto utilizamos corchetes no parentesis
+
+print(cocina[0])
+
+# Mostrar de manera inversa
+
+print(cocina[-1])
+
+# Acceder a un rango
+
+print(cocina[0:1])
+
+# EJEMPLO
+
+verduras = ("papa",)  # una tupla necesita aunque sea de un elemento la coma (,)
+# de lo contrario solo seria un tipo string cadena
+
+# video 7
+
+for cocinar in cocina:  # print esta usando \n para saltos de linea
+    print(cocinar, end=" ")  # Usamos end= para eliminar saltos de linea
+
+"""cocina[0] = "plato"  # da error
+print(cocina)
+
+cocinalista = list(cocina)
+cocinalista[0] = "plato"
+cocina = tuple(cocinalista)
+print("\n", cocina)
+
+del cocina  # esto es para eliminar
+print(cocina)
+"""
