fb153e006ab9a14f572af1d146b1960d3ae9b385
Matias Gonzalez <Skrowyy@gmail.com>
2023-10-30 20:44:35 -0300

    clase en java sobre argumentos y operador this

--- java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Operaciones;
 
-/**
- *
- * @author SKROWY
- */
+
 public class Aritmetica {
+    //atributos de la clase
+    int a;
+    int b;
+    
+    //Metodo
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+    }
+    
+    public int sumarConArgumentos(int arg1, int arg2){
+        this.a = arg1; // el argumento a se asigna al atributo this.a
+        this.b = arg2;
+        //return a + b;
+        return this.sumarConRetorno(); //video 5 clase 5
+    }
     
 }
